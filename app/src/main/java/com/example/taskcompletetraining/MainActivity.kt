eb1abdb20085267a5dc87bd2938d08aec9992dd5
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-01 01:04:54 +0200

    refact: delete useless codelines

--- app/src/main/java/com/example/taskcompletetraining/MainActivity.kt
+++ app/src/main/java/com/example/taskcompletetraining/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,10 +18,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskCompleteTrainingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
@@ -31,17 +25,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TaskCompleteTrainingTheme {
-        Greeting("Android")
     }
 }
