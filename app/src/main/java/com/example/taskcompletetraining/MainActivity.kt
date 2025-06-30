f65733228118f36e4b6638b498f2354ecf4faf73
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-01 01:15:49 +0200

    feat: add text

--- app/src/main/java/com/example/taskcompletetraining/MainActivity.kt
+++ app/src/main/java/com/example/taskcompletetraining/MainActivity.kt
@@ -9,10 +9,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskcompletetraining.ui.theme.TaskCompleteTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +40,19 @@ fun TaskCompleteLogo(modifier: Modifier = Modifier) {
     Column {
         val logo = painterResource(R.drawable.ic_task_completed)
 
-        Column {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Image(
                 painter = logo,
                 contentDescription = null,
+            )
+            Text(
+                text = stringResource(R.string.all_tasks_completed),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            )
+            Text(
+                text = stringResource(R.string.nice_work),
+                fontSize = 16.sp,
             )
         }
     }
