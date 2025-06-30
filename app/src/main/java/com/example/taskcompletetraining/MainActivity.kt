5d2b6df70350a821b0dd127f1784344d97cc073f
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-01 01:08:26 +0200

    feat: place logo

--- app/src/main/java/com/example/taskcompletetraining/MainActivity.kt
+++ app/src/main/java/com/example/taskcompletetraining/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskcompletetraining.ui.theme.TaskCompleteTrainingTheme
 
@@ -18,6 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskCompleteTrainingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TaskCompleteLogo(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -25,10 +30,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun TaskCompleteLogo(modifier: Modifier = Modifier) {
+    Column {
+        val logo = painterResource(R.drawable.ic_task_completed)
+
+        Column {
+            Image(
+                painter = logo,
+                contentDescription = null,
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskCompleteLogoPreview() {
     TaskCompleteTrainingTheme {
+        TaskCompleteLogo()
     }
 }
